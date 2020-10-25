6c5ffbab95b11bf96c8d36bd6fc5ae3e894cb264
creio <mailcreio@gmail.com>
2020-10-25 14:28:08 +0300

    25.10.2020 Update

---
+++ src/modules/baseinstall/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import subprocess
+import libcalamares
+from pathlib import Path
+
+root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+
+def run():
+    """
+    Installing base system!
+    """
+
+    try:
+     subprocess.call(['rm', '/tmp/chroot_path.tmp'])
+     with open('/tmp/chroot_path.tmp', 'w') as file:
+      root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+      file.write(root_mount_point)
+      file.close()
+    except:
+     pass
+
+    GET_PACSTRAP = "curl -s -Lo /tmp/pacstrap.sh https://raw.githubusercontent.com/ctlos/ctlos-sh/master/pacstrap.sh"
+    PAC_STRAP = "sh /tmp/pacstrap.sh"
+
+    try:
+        subprocess.call(GET_PACSTRAP.split(' '))
+        subprocess.call(PAC_STRAP.split(' '))
+    except:
+        pass
--- src/modules/keyboard/KeyboardPage.cpp
+++ src/modules/keyboard/KeyboardPage.cpp
@@ -35,7 +35,7 @@ class LayoutItem : public QListWidgetItem
 public:
     QString data;
 
-    virtual ~LayoutItem();
+    ~LayoutItem() override;
 };
 
 LayoutItem::~LayoutItem() {}
@@ -76,14 +76,12 @@ KeyboardPage::KeyboardPage( QWidget* parent )
     connect(
         ui->buttonRestore, &QPushButton::clicked, [this] { ui->comboBoxModel->setCurrentIndex( m_defaultIndex ); } );
 
-    connect( ui->comboBoxModel,
-             static_cast< void ( QComboBox::* )( const QString& ) >( &QComboBox::currentIndexChanged ),
-             [this]( const QString& text ) {
-                 QString model = m_models.value( text, "pc105" );
+    connect( ui->comboBoxModel, &QComboBox::currentTextChanged, [this]( const QString& text ) {
+        QString model = m_models.value( text, "pc105" );
 
-                 // Set Xorg keyboard model
-                 QProcess::execute( "setxkbmap", QStringList { "-model", model } );
-             } );
+        // Set Xorg keyboard model
+        QProcess::execute( "setxkbmap", QStringList { "-model", model } );
+    } );
 
     CALAMARES_RETRANSLATE( ui->retranslateUi( this ); )
 }
